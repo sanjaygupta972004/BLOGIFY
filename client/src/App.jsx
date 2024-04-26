e8df5f53347807ac7312942c9807f1aca7fefaa1
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2024-04-26 12:14:40 +0530

    Add flowbite-react and react-icons dependencies, and import Header component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Header from "./components/Header"
 import About from "./pages/About"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
@@ -9,7 +10,8 @@ import SignIn from "./pages/SignIn"
 
 function App() {
   return (
-       <BrowserRouter>
+    <BrowserRouter>
+      <Header/>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/sign-up" element={<SignUp />} />
